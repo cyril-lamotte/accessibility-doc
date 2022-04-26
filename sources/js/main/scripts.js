18235fe088bc622a506a84a8b7383aaf8df5f7ed
Cyril Lamotte <clamotte@jouve.com>
2022-04-26 17:21:39 +0200

    Layout & display script

--- sources/js/main/scripts.js
+++ sources/js/main/scripts.js
@@ -1,13 +1,26 @@
-const buttonList = document.querySelectorAll('.controls button');
-buttonList.forEach((button) => {
-  const id = button.getAttribute('id');
 
-  button.addEventListener('click', () => {
-    document.body.classList.toggle(id);
+// Get all checkboxes.
+const checkboxList = document.querySelectorAll('.criteria-radio input[type="radio"]');
+const form = document.querySelector('form');
+const hintList = document.querySelectorAll('.hint');
+
+checkboxList.forEach((cb) => {
+  // Get the id as reference.
+
+  cb.addEventListener('change', () => {
+    const id = cb.getAttribute('id');
+
+    // Hide all hints.
+    hintList.forEach((hint) => {
+      const ref = hint.getAttribute('data-a11y-ref');
+      hint.classList.remove('is-visible');
+      form.classList.remove(ref);
+    });
+
+    const relatedHintList = document.querySelectorAll(`.hint[data-a11y-ref="${id}"]`);
+    relatedHintList.forEach((hint) => {
+      hint.classList.add('is-visible');
+      form.classList.add(id);
+    });
   });
-});
-
-const toggleHints = document.getElementById('toggle-hints');
-toggleHints.addEventListener('click', () => {
-  document.body.classList.toggle('show-hints');
 });
