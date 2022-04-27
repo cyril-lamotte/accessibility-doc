01642bb6e08a7c602e614e519d9f971cf687a58b
Cyril Lamotte <clamotte@jouve.com>
2022-04-27 16:00:30 +0200

    Scroll to example

--- sources/js/main/scripts.js
+++ sources/js/main/scripts.js
@@ -17,10 +17,62 @@ checkboxList.forEach((cb) => {
       form.classList.remove(ref);
     });
 
+    const outlineList = form.querySelectorAll('.outline');
+    outlineList.forEach((outline) => {
+      outline.classList.remove('outline');
+    });
+
     const relatedHintList = document.querySelectorAll(`.hint[data-a11y-ref="${id}"]`);
     relatedHintList.forEach((hint) => {
       hint.classList.add('is-visible');
       form.classList.add(id);
+
+      let domSelector = null;
+
+      switch (id) {
+        case 'c11-1':
+          domSelector = 'label';
+          break;
+
+        case 'c11-5':
+          domSelector = '[role="group"]';
+          break;
+
+        case 'c11-8':
+          domSelector = 'select';
+          break;
+
+        case 'c11-9':
+          domSelector = 'button';
+          break;
+
+        case 'c11-10':
+          domSelector = 'input[required]';
+          break;
+
+        case 'c11-13':
+          domSelector = 'input[autocomplete]';
+          break;
+
+        default:
+          break;
+      }
+
+      if (domSelector) {
+        domList = form.querySelectorAll(domSelector);
+        domList.forEach((dom, i) => {
+          dom.classList.add('outline');
+
+          // Scroll to the first element.
+          if (i === 0) {
+            dom.scrollIntoView({
+              behavior: 'smooth',
+              block: 'center',
+            });
+          }
+        });
+      }
+
     });
   });
 });
